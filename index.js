48c676e98f6bf209480b72c06bd1a1aa35bed799
jayblacc2 <jideman2k@yandex.ru>
2022-11-14 07:57:03 +0400

    password text-content

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const form = document.getElementById('form'),
   password = document.querySelector('input[type="password"]'),
-  confirm_password = document.querySelector('.confirm-password'),
- 
+  confirm_password = document.querySelector('.confirm-password')
 
 function check() {
-  let message = document.querySelector('.message');
+  let message = document.querySelector('.message')
   if (password.value === confirm_password.value) {
-    message.innerHTML = 'Password matched';
-    message.style.color = 'green';
+    message.textContent = 'Password matched'
+    message.style.color = 'green'
   } else {
-    message.innerHTML = 'Password not matched';
-    message.style.color = 'red';
+    message.textContent = 'Password not matched'
+    message.style.color = 'red'
   }
 }
+
+form.addEventListener('submit', (e) => e.preventDefault())
